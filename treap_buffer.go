90fc9f5ac9c9c5868010543783446517a9ebf4b4
Matija Krajnik <matija.krajnik90@gmail.com>
2024-06-08 16:45:31 +0200

    initial implementation with tests

---
+++ treap_buffer.go
@@ -0,0 +1,162 @@
+package main
+
+import "fmt"
+
+type treapBuffer struct {
+	root        *node
+	maxSize     int
+	currentSize int
+}
+
+type node struct {
+	value    int
+	priority float64
+	left     *node
+	right    *node
+}
+
+func newNode(value int, priority float64) *node {
+	return &node{
+		value:    value,
+		priority: priority,
+		left:     nil,
+		right:    nil,
+	}
+}
+
+func newTreapBuffer(maxSize int) *treapBuffer {
+	return &treapBuffer{
+		root:        nil,
+		maxSize:     maxSize,
+		currentSize: 0,
+	}
+}
+
+func rightRotate(node *node) *node {
+	pivot := node.left
+	temp := pivot.right
+	pivot.right = node
+	node.left = temp
+	return pivot
+}
+
+func leftRotate(node *node) *node {
+	pivot := node.right
+	temp := pivot.left
+	pivot.left = node
+	node.right = temp
+	return pivot
+}
+
+func (tb *treapBuffer) insert(newNode *node) {
+	if tb.contains((newNode.value)) {
+		tb.delete(newNode.value)
+	}
+
+	tb.root = insertNode(tb.root, newNode)
+	tb.currentSize++
+}
+
+func insertNode(root, newNode *node) *node {
+	if root == nil {
+		return newNode
+	}
+
+	if root.value > newNode.value {
+		root.left = insertNode(root.left, newNode)
+		if root.priority < newNode.priority {
+			return rightRotate(root)
+		}
+		return root
+	} else if root.value < newNode.value {
+		root.right = insertNode(root.right, newNode)
+		if root.priority < newNode.priority {
+			return leftRotate(root)
+		}
+		return root
+	}
+
+	return root
+}
+
+func (tb *treapBuffer) delete(value int) {
+	root, deleted := deleteNode(tb.root, value, false)
+	tb.root = root
+	if deleted {
+		tb.currentSize--
+	}
+}
+
+func deleteNode(root *node, value int, found bool) (*node, bool) {
+	if root == nil {
+		return root, found
+	}
+
+	switch {
+	case value < root.value:
+		root.left, found = deleteNode(root.left, value, found)
+	case value > root.value:
+		root.right, found = deleteNode(root.right, value, found)
+	case value == root.value:
+		switch {
+		case root.left == nil:
+			root = root.right
+		case root.right == nil:
+			root = root.left
+		default:
+			if root.left.priority < root.right.priority {
+				root = leftRotate(root)
+				root.left, _ = deleteNode(root.left, value, found)
+			} else {
+				root = rightRotate(root)
+				root.right, _ = deleteNode(root.right, value, found)
+			}
+		}
+		found = true
+	}
+
+	return root, found
+}
+
+func (tb *treapBuffer) contains(value int) bool {
+	current := tb.root
+
+	for current != nil {
+		if value == current.value {
+			return true
+		}
+
+		if value < current.value {
+			current = current.left
+		} else {
+			current = current.right
+		}
+	}
+
+	return false
+}
+
+func (tb *treapBuffer) printBasicInfo() {
+	fmt.Printf("Size: %d\n", tb.currentSize)
+	fmt.Printf("Root: ")
+	printNode(tb.root)
+}
+
+// func (tb *treapBuffer) printDetails() {
+// 	fmt.Printf("Size: %d\n", tb.currentSize)
+// 	fmt.Printf("Root: ")
+// 	printNode(tb.root)
+// 	printFrom((tb.root))
+// }
+
+// func printFrom(node *node) {
+// 	if node != nil {
+// 		printFrom(node.left)
+// 		printNode(node)
+// 		printFrom(node.right)
+// 	}
+// }
+
+func printNode(node *node) {
+	fmt.Printf("<Value: %d, Priority: %f>\n", node.value, node.priority)
+}
---
+++ treap_buffer_test.go
@@ -0,0 +1,323 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("TreapBuffer", func(t *testing.T) {
+		buffer := newTreapBuffer(10)
+		assert.Nil(t, buffer.root)
+		assert.Equal(t, 0, buffer.currentSize)
+		assert.Equal(t, 10, buffer.maxSize)
+	})
+
+	t.Run("Node", func(t *testing.T) {
+		node := newNode(123, 0.456)
+		assert.Equal(t, 123, node.value)
+		assert.Equal(t, 0.456, node.priority)
+		assert.Nil(t, node.left)
+		assert.Nil(t, node.right)
+	})
+}
+
+func TestInsert(t *testing.T) {
+	buffer := newTreapBuffer(10)
+
+	t.Run("OnEmpty", func(t *testing.T) {
+		buffer.insert(newNode(30, 0.200))
+		assert.Equal(t, 1, buffer.currentSize)
+		assert.Equal(t, 30, buffer.root.value)
+		assert.Equal(t, 0.200, buffer.root.priority)
+	})
+
+	t.Run("NewRightLeaf", func(t *testing.T) {
+		buffer.insert(newNode(40, 0.100))
+		assert.Equal(t, 2, buffer.currentSize)
+		assert.Equal(t, 30, buffer.root.value)
+		assert.Equal(t, 0.200, buffer.root.priority)
+		assert.Nil(t, buffer.root.left)
+		assert.Equal(t, 40, buffer.root.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.priority)
+	})
+
+	t.Run("NewRoot", func(t *testing.T) {
+		buffer.insert(newNode(20, 0.300))
+		assert.Equal(t, 3, buffer.currentSize)
+		assert.Equal(t, 20, buffer.root.value)
+		assert.Equal(t, 0.300, buffer.root.priority)
+		assert.Nil(t, buffer.root.left)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.priority)
+	})
+
+	t.Run("NewLeftLeaf", func(t *testing.T) {
+		buffer.insert(newNode(10, 0.210))
+		assert.Equal(t, 4, buffer.currentSize)
+		assert.Equal(t, 20, buffer.root.value)
+		assert.Equal(t, 0.300, buffer.root.priority)
+		assert.Equal(t, 10, buffer.root.left.value)
+		assert.Equal(t, 0.210, buffer.root.left.priority)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.priority)
+	})
+
+	t.Run("MiddleWithRotate", func(t *testing.T) {
+		buffer.insert(newNode(15, 0.220))
+		assert.Equal(t, 5, buffer.currentSize)
+		assert.Equal(t, 20, buffer.root.value)
+		assert.Equal(t, 0.300, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 10, buffer.root.left.left.value)
+		assert.Equal(t, 0.210, buffer.root.left.left.priority)
+		assert.Nil(t, buffer.root.left.right)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.priority)
+	})
+
+	t.Run("NewRootLowerValue", func(t *testing.T) {
+		buffer.insert(newNode(18, 0.310))
+		assert.Equal(t, 6, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 20, buffer.root.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.priority)
+		assert.Equal(t, 10, buffer.root.left.left.value)
+		assert.Equal(t, 0.210, buffer.root.left.left.priority)
+		assert.Nil(t, buffer.root.left.right)
+		assert.Equal(t, 30, buffer.root.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.right.priority)
+	})
+}
+
+func BenchmarkInsert(b *testing.B) {
+	lengths := []int{1_000, 10_000, 100_000, 1_000_000}
+	for _, length := range lengths {
+		b.Run(fmt.Sprintf("%d", length), func(b *testing.B) {
+			stream := generateRepeatingStream(length, length)
+			buffer := newTreapBuffer(length)
+			b.ResetTimer()
+			for _, element := range stream {
+				buffer.insert(newNode(element, rand.Float64()))
+			}
+		})
+	}
+}
+
+func TestInsertOverwrite(t *testing.T) {
+	buffer := newTreapBuffer(10)
+	buffer.insert(newNode(30, 0.200))
+	buffer.insert(newNode(40, 0.100))
+	buffer.insert(newNode(20, 0.300))
+	buffer.insert(newNode(10, 0.210))
+	buffer.insert(newNode(15, 0.220))
+	buffer.insert(newNode(18, 0.310))
+
+	newPriority1 := 0.230
+	t.Run("InTheMiddle", func(t *testing.T) {
+		buffer.insert(newNode(10, newPriority1))
+		assert.Equal(t, 6, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 10, buffer.root.left.value)
+		assert.Equal(t, newPriority1, buffer.root.left.priority)
+		assert.Equal(t, 20, buffer.root.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.left.left)
+		assert.Equal(t, 15, buffer.root.left.right.value)
+		assert.Equal(t, 0.220, buffer.root.left.right.priority)
+		assert.Equal(t, 30, buffer.root.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.right.priority)
+	})
+
+	newPriority2 := 0.350
+	t.Run("UpgradeToRoot", func(t *testing.T) {
+		buffer.insert(newNode(15, newPriority2))
+		assert.Equal(t, 6, buffer.currentSize)
+		assert.Equal(t, 15, buffer.root.value)
+		assert.Equal(t, newPriority2, buffer.root.priority)
+		assert.Equal(t, 10, buffer.root.left.value)
+		assert.Equal(t, newPriority1, buffer.root.left.priority)
+		assert.Equal(t, 18, buffer.root.right.value)
+		assert.Equal(t, 0.310, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 20, buffer.root.right.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.right.priority)
+		assert.Nil(t, buffer.root.right.right.left)
+		assert.Equal(t, 30, buffer.root.right.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.right.priority)
+		assert.Nil(t, buffer.root.right.right.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.right.right.priority)
+	})
+
+	newPriority3 := 0.150
+	t.Run("DowngradeFromRoot", func(t *testing.T) {
+		buffer.insert(newNode(15, newPriority3))
+		assert.Equal(t, 6, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 10, buffer.root.left.value)
+		assert.Equal(t, newPriority1, buffer.root.left.priority)
+		assert.Nil(t, buffer.root.left.left)
+		assert.Equal(t, 15, buffer.root.left.right.value)
+		assert.Equal(t, newPriority3, buffer.root.left.right.priority)
+		assert.Equal(t, 20, buffer.root.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Equal(t, 30, buffer.root.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.priority)
+		assert.Nil(t, buffer.root.right.right.left)
+		assert.Equal(t, 40, buffer.root.right.right.right.value)
+		assert.Equal(t, 0.100, buffer.root.right.right.right.priority)
+		assert.Nil(t, buffer.root.right.right.right.left)
+	})
+}
+
+func BenchmarkInsertOverwrite(b *testing.B) {
+	lengths := []int{1_000, 10_000, 100_000, 1_000_000}
+	for _, length := range lengths {
+		b.Run(fmt.Sprintf("%d", length), func(b *testing.B) {
+			stream := generateRepeatingStream(length, length/100)
+			buffer := newTreapBuffer(length)
+			for i := 0; i < length/100; i++ {
+				buffer.insert(newNode(stream[i], rand.Float64()))
+			}
+			b.ResetTimer()
+			for _, element := range stream {
+				buffer.insert(newNode(element, rand.Float64()))
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	buffer := newTreapBuffer(10)
+	buffer.insert(newNode(30, 0.200))
+	buffer.insert(newNode(40, 0.100))
+	buffer.insert(newNode(20, 0.300))
+	buffer.insert(newNode(10, 0.210))
+	buffer.insert(newNode(15, 0.220))
+	buffer.insert(newNode(18, 0.310))
+
+	t.Run("RightLeaf", func(t *testing.T) {
+		buffer.delete(40)
+		assert.Equal(t, 5, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 20, buffer.root.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.priority)
+		assert.Equal(t, 10, buffer.root.left.left.value)
+		assert.Equal(t, 0.210, buffer.root.left.left.priority)
+		assert.Nil(t, buffer.root.left.right)
+		assert.Equal(t, 30, buffer.root.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.priority)
+		assert.Nil(t, buffer.root.right.left)
+		assert.Nil(t, buffer.root.right.right.right)
+	})
+
+	t.Run("LeftLeaf", func(t *testing.T) {
+		buffer.delete(10)
+		assert.Equal(t, 4, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 20, buffer.root.right.value)
+		assert.Equal(t, 0.300, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.left.left)
+		assert.Nil(t, buffer.root.left.right)
+		assert.Equal(t, 30, buffer.root.right.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.right.priority)
+	})
+
+	t.Run("Middle", func(t *testing.T) {
+		buffer.delete(20)
+		assert.Equal(t, 3, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.right)
+	})
+
+	t.Run("NotExisting", func(t *testing.T) {
+		buffer.delete(20)
+		assert.Equal(t, 3, buffer.currentSize)
+		assert.Equal(t, 18, buffer.root.value)
+		assert.Equal(t, 0.310, buffer.root.priority)
+		assert.Equal(t, 15, buffer.root.left.value)
+		assert.Equal(t, 0.220, buffer.root.left.priority)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+		assert.Nil(t, buffer.root.right.right)
+	})
+
+	t.Run("Root", func(t *testing.T) {
+		buffer.delete(18)
+		assert.Equal(t, 2, buffer.currentSize)
+		assert.Equal(t, 15, buffer.root.value)
+		assert.Equal(t, 0.220, buffer.root.priority)
+		assert.Nil(t, buffer.root.left)
+		assert.Equal(t, 30, buffer.root.right.value)
+		assert.Equal(t, 0.200, buffer.root.right.priority)
+	})
+
+	t.Run("LastLeafOnRoot", func(t *testing.T) {
+		buffer.delete(30)
+		assert.Equal(t, 1, buffer.currentSize)
+		assert.Equal(t, 15, buffer.root.value)
+		assert.Equal(t, 0.220, buffer.root.priority)
+		assert.Nil(t, buffer.root.left)
+		assert.Nil(t, buffer.root.right)
+	})
+
+	t.Run("LastRoot", func(t *testing.T) {
+		buffer.delete(15)
+		assert.Equal(t, 0, buffer.currentSize)
+		assert.Nil(t, buffer.root)
+	})
+}
+
+func BenchmarkContains(b *testing.B) {
+	lengths := []int{1_000, 10_000, 100_000, 1_000_000}
+	for _, length := range lengths {
+		b.Run(fmt.Sprintf("%d", length), func(b *testing.B) {
+			stream := generateRepeatingStream(length, length-1)
+			buffer := newTreapBuffer(length)
+			for i := 0; i < length; i++ {
+				buffer.insert(newNode(stream[i], rand.Float64()))
+			}
+			b.ResetTimer()
+			for _, element := range stream {
+				buffer.contains(element)
+			}
+		})
+	}
+}
