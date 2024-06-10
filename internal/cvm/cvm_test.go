a649b08f38981ef12c480808a2ece1d067c07072
Matija Krajnik <matija.krajnik90@gmail.com>
2024-06-10 20:43:50 +0200

    cvm run tests

---
+++ internal/cvm/cvm_test.go
@@ -0,0 +1,40 @@
+package cvm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tentameneu/cvm-go/internal/config"
+	"github.com/tentameneu/cvm-go/internal/stream"
+)
+
+func newTestRepeatingStreamRunner(conf *config.Config) *CVMRunner {
+	streamGenerator, _ := stream.NewStreamGenerator(conf)
+	return NewCVMRunner(streamGenerator.Generate(), conf.GetBufferSize())
+}
+
+func TestRun(t *testing.T) {
+	t.Run("SmallerBuffer", func(t *testing.T) {
+		conf, _ := config.NewConfig(map[string]any{
+			"genType":    "repeating",
+			"total":      1_000_000,
+			"distinct":   10_000,
+			"bufferSize": 1_000,
+		})
+		runner := newTestRepeatingStreamRunner(conf)
+		n := runner.Run()
+		assert.InDelta(t, 10_000, n, 1_000)
+	})
+
+	t.Run("ExactBuffer", func(t *testing.T) {
+		conf, _ := config.NewConfig(map[string]any{
+			"genType":    "repeating",
+			"total":      1_000_000,
+			"distinct":   10_000,
+			"bufferSize": 10_000,
+		})
+		runner := newTestRepeatingStreamRunner(conf)
+		n := runner.Run()
+		assert.Exactly(t, 10_000, n)
+	})
+}
