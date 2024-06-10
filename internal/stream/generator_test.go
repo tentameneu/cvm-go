b19ec8f6fe74082ec2317687c420ca2511d1c2f0
Matija Krajnik <matija.krajnik90@gmail.com>
2024-06-10 14:11:34 +0200

    generate repeating stream unit test

--- internal/stream/generator_test.go
+++ internal/stream/generator_test.go
@@ -34,3 +34,30 @@ func TestNewStreamGenerator(t *testing.T) {
 		assert.Error(t, err, ("unknown generator type 'unknown'"))
 	})
 }
+
+func TestStreamGenerate(t *testing.T) {
+	t.Run("Repeating", func(t *testing.T) {
+		conf, _ := config.NewConfig(map[string]any{
+			"genType":    "repeating",
+			"total":      10,
+			"distinct":   5,
+			"bufferSize": 10,
+		})
+		generator, err := NewStreamGenerator(conf)
+		assert.NotNil(t, generator)
+		assert.Nil(t, err)
+
+		stream := generator.Generate()
+		assert.Equal(t, 10, len(stream))
+		assert.Equal(t, 0, stream[0])
+		assert.Equal(t, 1, stream[1])
+		assert.Equal(t, 2, stream[2])
+		assert.Equal(t, 3, stream[3])
+		assert.Equal(t, 4, stream[4])
+		assert.Equal(t, 0, stream[5])
+		assert.Equal(t, 1, stream[6])
+		assert.Equal(t, 2, stream[7])
+		assert.Equal(t, 3, stream[8])
+		assert.Equal(t, 4, stream[9])
+	})
+}
