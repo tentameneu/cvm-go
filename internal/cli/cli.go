102ea4a08da0f933e0a3e63edae2f07b87e631ca
Matija Krajnik <matija.krajnik90@gmail.com>
2024-06-18 18:56:16 +0200

    minor typo fixes

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -9,7 +9,7 @@ import (
 
 var streamType = flag.String("stream-type", "incremental", "how to generate test stream of elements. valid values are: [incremental, random, file]")
 var total = flag.Int("total", 100_000_000, "total number of elements in generated test stream")
-var distinct = flag.Int("distinct", 5_000_000, "number of distincts elements in generated test stream")
+var distinct = flag.Int("distinct", 5_000_000, "number of distinct elements in generated test stream")
 var randomMin = flag.Int("random-min", 0, "used in random stream generator - generates values in range [random-min, random-max]")
 var randomMax = flag.Int("random-max", 10_000_000, "used in random stream generator - generates values in range [random-min, random-max]")
 var bufferSize = flag.Int("buffer-size", 10_000, "number of elements that can be stored in buffer while processing stream")
@@ -39,7 +39,7 @@ func usage() {
 	fmt.Print(`
 This program runs CVM algorithm simulator.
 
-Algorithm estimates number of distinct elments in stream of elements much bigger than available buffer size.
+Algorithm estimates number of distinct elements in stream of elements much bigger than available buffer size.
 
 Usage:
 
